ae8323002c846719b1eb5b64e4cb6f768a2e8da9
TrongThang <it.ptthang@gmail.com>
2024-12-27 20:36:05 +0700

    update deviceCard with data static

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './App.css'
 import './resource/css/style.css';
-import './resource/css/index.css';
 // <!-- Libraries Stylesheet -->
 import './resource/lib/animate/animate.min.css'
 import './resource/lib/lightbox/css/lightbox.min.css'
 import './resource/lib/owlcarousel/assets/owl.carousel.min.css'
-  //  <!-- Icon Font Stylesheet -->
 // <!-- Customized Bootstrap Stylesheet -->
 import './resource/css/bootstrap.min.css'
-import './resource/css/index.css'
+
 // <!-- Template Stylesheet -->
+
 import './resource/css/style.css'
+import './resource/css/inq.css';
+
 import DeviceCard from './component/Shared/deviceCard';
 
 function App() {
---
+++ front-end/src/component/Shared/readme.md
@@ -0,0 +1,4 @@
+Chứa các component dùng chung
+- Thẻ sản phẩm
+- list lịch sử xem (Hiện tại chưa có) 
+- Phân trang

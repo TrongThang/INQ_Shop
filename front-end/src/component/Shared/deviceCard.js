1613c0344bb109d6fe3fe057840c022ccdb8f2ac
TrongThang <it.ptthang@gmail.com>
2024-12-29 19:02:54 +0700

    Revert "update deviceCard with data static"
    This reverts commit ae8323002c846719b1eb5b64e4cb6f768a2e8da9.

--- front-end/src/component/Shared/deviceCard.js
+++ front-end/src/component/Shared/deviceCard.js
@@ -1,60 +1,55 @@
 import { useEffect, useState } from "react";
 
-export default function DeviceCard() {
-    const data = {
-        image: 'https://placehold.co/200x100',
-        name: 'Đèn thông minh',
-        sellingPrice: '500,000 VNĐ',
-        description: 'Kiểm soát ánh sáng linh hoạt, phù hợp với nhu cầu sử dụng và tiết kiệm năng lượng cho gia đình bạn.',
-        starRating: 4.5,
-    }
-    const [device, setDevice] = useState(data);
+const DeviceCard = () => {
+    const [device, setDevice] = useState(null);
     const [loadingDevice, setLoadingDevice] = useState(false);
 
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         try {
-    //             setLoadingDevice(true); // Bắt đầu tải dữ liệu
-    //             const response = await fetch('https://api.example.com/v1/api/getDeivceById/id');
-    //             const result = await response.json();
-    //             setDevice(result); // Cập nhật state với dữ liệu API
-    //         } catch (err) {
-    //             console.error(err);
-    //         } finally {
-    //             setLoadingDevice(false); // Dừng trạng thái tải
-    //         }
-    //     };
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoadingDevice(true); // Bắt đầu tải dữ liệu
+                const response = await fetch('https://api.example.com/v1/api/getDeivceById/id');
+                const result = await response.json();
+                setDevice(result); // Cập nhật state với dữ liệu API
+            } catch (err) {
+                console.error(err);
+            } finally {
+                setLoadingDevice(false); // Dừng trạng thái tải
+            }
+        };
 
-    //     if (loadingDevice) {
-    //         fetchData();
-    //     }
-    // }, [loadingDevice]); // Chỉ chạy khi `loadingDevice` thay đổi
+        if (loadingDevice) {
+            fetchData();
+        }
+    }, [loadingDevice]); // Chỉ chạy khi `loadingDevice` thay đổi
 
-    // const handleLoadDevice = () => {
-    //     setLoadingDevice(true); // Kích hoạt tải dữ liệu
-    // };
+    const handleLoadDevice = () => {
+        setLoadingDevice(true); // Kích hoạt tải dữ liệu
+    };
 
     return (
         <div className="col-xl-2 col-lg-2 product-items wow fadeInUp" data-wow-delay="0.2s">
             <div className="service-item ">
             <div className="service-img">
                 <div className="img-change">
-                <img src={device.image} className="img-fluid rounded-top w-100" alt=""/>
+                <img src="https://placehold.co/200x100" className="img-fluid rounded-top w-100" alt="Smart Lighting"/>
                 {/* device.image thay vào nội dung của */}
                 </div>
             </div>
             <div className="service-content p-4">
                 <div className="service-content-inner">
-                    <a href="#" className="d-inline-block h4 mb-2 ">{ device.name }</a> 
-                    <p className="mb-2 text-primary fw-bold">{ device.sellingPrice }</p>
-                    <p className="mb-2">⭐⭐⭐⭐⭐ ({ device.starRating }/5)</p>
-                    <p className="mb-4 line-clamp-p">
-                        { device.description}
-                    </p>
-                    <a className="btn btn-primary rounded-pill py-2 px-4" href="#">Chi tiết</a>
+                <a href="#" className="d-inline-block h4 mb-2 ">Đèn Thông Minh Minh</a> 
+                <p className="mb-2 text-primary fw-bold">500,000 VND</p>
+                <p className="mb-2">⭐⭐⭐⭐⭐ (4.5/5)</p>
+                <p className="mb-4 line-clamp-p">
+                    Kiểm soát ánh sáng linh hoạt, phù hợp với nhu cầu sử dụng và tiết kiệm
+                    năng lượng cho gia đình bạn.
+                </p>
+                <a className="btn btn-primary rounded-pill py-2 px-4" href="#">Chi tiết</a>
                 </div>
             </div>
             </div>
         </div>
     )
 }  
+export default DeviceCard;
--- front-end/src/component/Shared/readme.md
+++
@@ -1,4 +0,0 @@
-Chứa các component dùng chung
-- Thẻ sản phẩm
-- list lịch sử xem (Hiện tại chưa có) 
-- Phân trang

67b5baf5eebd509b2d38d221e9bd2d5dbe1ecf30
Minh Nhan <tlhoangminhnhan2004@gmail.com>
2024-12-31 13:59:28 +0700

    Update giao diện

--- front-end/src/component/LayoutCustomer/Header/header.js
+++ front-end/src/component/LayoutCustomer/Header/header.js
@@ -2,8 +2,10 @@ import Navbar from "./navbar";
 
 export default function Header() {
     return (
-        <div class="container-fluid nav-bar px-0 px-lg-4 py-lg-0">
-            <Navbar />
+        <div class="container-fluid nav-bar px-0 px-lg-4 py-lg-0 mb-2">
+            <div class="container">
+                <Navbar />
+            </div>
         </div>
     );
 }

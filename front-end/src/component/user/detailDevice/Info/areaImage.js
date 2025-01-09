c26eed6a6e26b68dd0829f3cf2ff8efb7cb3238a
TrongThang <it.ptthang@gmail.com>
2025-01-09 19:51:18 +0700

    fix image cart, detail device

--- front-end/src/component/user/detailDevice/Info/areaImage.js
+++ front-end/src/component/user/detailDevice/Info/areaImage.js
@@ -1,16 +1,11 @@
-export default function AreaImage({ imagesDevice }) {
-    imagesDevice = imagesDevice ?? [
-        "https://placehold.co/75x75",
-        "https://placehold.co/75x75",
-        "https://placehold.co/75x75",
-    ];
+export default function AreaImage({image}) {
     
     return (
-        <div className="col-xl-5 wow fadeInLeft mb-3" data-wow-delay="0.4s">
-            <img src="https://placehold.co/600x400" className="img-fluid w-100 py-3" alt=""/>
-            <div className="thumbnail-row d-flex justify-content-start">
+        <div className="col-xl-5 wow fadeInLeft mb-3 border border-dark" data-wow-delay="0.4s">
+            <img src={`/img/device/${image}` || "https://placehold.co/600x400"} className="img-fluid w-100 py-3" alt=""/>
+            {/* <div className="thumbnail-row d-flex justify-content-start">
                 {imagesDevice.map(image => (<img src={image} className="img-fluid p-1" alt="Thumbnail" />))}
-            </div>
+            </div> */}
         </div>
     ); 
 }

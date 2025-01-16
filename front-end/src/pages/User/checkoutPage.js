050d60ad740565558ee21a077760dbd5f1578801
TrongThang <it.ptthang@gmail.com>
2025-01-17 01:10:07 +0700

    update checkoutPage

--- front-end/src/pages/User/checkoutPage.js
+++ front-end/src/pages/User/checkoutPage.js
@@ -1,16 +1,27 @@
 import { useEffect, useState } from "react";
 import InfoOrder from "../../component/user/Checkout/infoOrder";
 import ListDeviceOrder from "../../component/user/Checkout/listDeviceOrder";
-import { ToastContainer } from "react-toastify";
 import { useCart } from "../../context/CartContext";
+import { useNavigate } from "react-router-dom";
 
 export default function CheckoutPage() {
+    const navigate = useNavigate()
     const { cart } = useCart();
     const [phone, setPhone] = useState('');
     const [shippingMethod, setShippingMethod] = useState('COD');
     const [notes, setNotes] = useState('');
     const [choiceAddress, setChoiceAddress] = useState();
     const [deviceCheckout, setDeviceCheckout] = useState([])
+
+    
+    useEffect(() => {
+        const filteredCart = cart.filter(item => item.status && item.stock >= item.quantity); 
+            console.log('length:', filteredCart.length)
+            if (filteredCart.length <= 0) {
+                navigate('/cart')
+                return;
+            }
+    }, [])
 
     useEffect(() => {
         const filteredCart = cart.filter(item => item.status && item.stock >= item.quantity);

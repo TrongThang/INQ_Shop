9fdebd7018487e07786ddf66aa0ac6c2cbdc5e41
tuan6525 <trantuan6525@gmail.com>
2025-02-10 20:56:02 +0700

    update SearchProduct

--- front-end/src/component/Admin/Mana_product/searchProduct.js
+++ front-end/src/component/Admin/Mana_product/searchProduct.js
@@ -1,5 +1,5 @@
 import React, { use, useState } from "react";
-export default function SearchCategory({onSearch}) {
+export default function SearchCategory({onFilter, onSearch}) {
     const [searchTerm, setSearchTerm] = useState(""); // State để lưu từ khóa tìm kiếm
    
     // Xử lý khi người dùng nhập từ khóa
@@ -9,6 +9,7 @@ export default function SearchCategory({onSearch}) {
         onSearch(value); // Gọi hàm callback từ component cha
     };
     return (
+        <>
         <div class="row mb-3">
             <div class="col-md-6">
                 <div class="input-group">
@@ -22,4 +23,26 @@ export default function SearchCategory({onSearch}) {
                 </div>
             </div>            
 	</div>
+    <div className="row mb-3">
+    <div className="col-md-6">
+        <div className="input-group">
+            <span className="input-group-text">
+                <i className="bi bi-search"></i>
+            </span>
+            <input type="text" className="form-control" placeholder="Tìm kiếm sản phẩm" onChange={(e) => onSearch(e.target.value)}/>
+        </div>
+    </div>
+    <div className="col-md-2">
+        <select className="form-select" onChange={(e) => onFilter(e.target.value)}>
+            <option value={"6"}>Tất cả</option>
+            <option value={"1"}>Đang bán</option>
+            <option value={"2"}>Khuyến mãi</option>
+            <option value={"3"}>Nổi bật</option>
+            <option value={"4"}>Mới</option>
+            <option value={"5"}>Bán chạy</option>
+            <option value={"0"}>Ngừng bán</option>
+        </select>
+    </div>
+</div>
+</>
 )}
